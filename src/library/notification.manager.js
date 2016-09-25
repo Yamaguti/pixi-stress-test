e4d04ef0523a068928e8b9e6dd36af89041ec546
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 16:15:01 -0300

    Adding semi colon to notification.manager

--- src/library/notification.manager.js
+++ src/library/notification.manager.js
@@ -5,36 +5,35 @@ lib_.notificationManager.registerTable = {}
 
 
 lib_.notificationManager.register = function(notification, callback) {
-    lib_.notificationManager.registerTable[notification] = lib_.notificationManager.registerTable[notification] || []
-    var listenerArray = lib_.notificationManager.registerTable[notification]
+    lib_.notificationManager.registerTable[notification] = (lib_.notificationManager.registerTable[notification] || []);
+    var listenerArray = lib_.notificationManager.registerTable[notification];
 
-    listenerArray.push(callback)
+    listenerArray.push(callback);
 }
 
 
 lib_.notificationManager.deregister = function(notification, callback) {
-    lib_.notificationManager.registerTable[notification] = lib_.notificationManager.registerTable[notification] || []
-    var listenerArray = lib_.notificationManager.registerTable[notification]
+    lib_.notificationManager.registerTable[notification] = (lib_.notificationManager.registerTable[notification] || []);
+    var listenerArray = lib_.notificationManager.registerTable[notification];
 
-    lib_.utils.removeFromArray(listenerArray, callback)
+    lib_.utils.removeFromArray(listenerArray, callback);
 
     // Remove array if empty
-    if (listenerArray.length == 0) {
-        delete lib_.notificationManager.registerTable[notification]
-    }
+    if (listenerArray.length == 0)
+        delete lib_.notificationManager.registerTable[notification];
 }
 
 
 lib_.notificationManager.notify = function(notification) {
-    lib_.notificationManager.registerTable[notification] = lib_.notificationManager.registerTable[notification] || []
-    var listenerArray = lib_.notificationManager.registerTable[notification]
-    var results = []
+    lib_.notificationManager.registerTable[notification] = (lib_.notificationManager.registerTable[notification] || []);
+    var listenerArray = lib_.notificationManager.registerTable[notification];
+    var results = [];
 
     var notificationIndex;
     for (notificationIndex = listenerArray.length-1; notificationIndex >= 0; notificationIndex--) {
-        var callback = listenerArray[notificationIndex]
-        results.push(callback(arguments[1]))
+        var callback = listenerArray[notificationIndex];
+        results.push(callback(arguments[1]));
     }
 
-    return results
+    return results;
 }
