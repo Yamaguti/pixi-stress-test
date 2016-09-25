20f8d0209066d2abd6847a7a343b2c0aafa77115
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 19:57:59 -0300

    Adding TODO

--- src/library/lib.debug.js
+++ src/library/lib.debug.js
@@ -8,3 +8,7 @@ lib_.debug.noop = function() { }
 
 lib_.debug.updateBegin = lib_.debug.noop
 lib_.debug.updateEnd   = lib_.debug.noop
+
+// TODO expose an interface to register and deregister
+// callbacks to play at updateBegin and updateEnd. Instead
+// of switching references like I did.
