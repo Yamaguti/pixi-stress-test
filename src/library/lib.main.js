f491c42b25d2f0f9b483cafcd496f4d1bcb210b6
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 18:15:18 -0300

    Organizing setup function.

--- src/library/lib.main.js
+++ src/library/lib.main.js
@@ -1,2 +1,44 @@
 
 var lib_ = {}
+
+
+lib_.logicUpdate = function(dt) {
+    // Phisics update
+    // Timer Update
+    // Transition Update
+
+    // Custom game update
+}
+
+lib_.setUp = function() {
+    // Creates global stage
+    stage = new PIXI.Container();
+
+    // Creates a renderer instance.
+    renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
+    document.body.appendChild(renderer.view);
+
+    // Sets update loop
+    var lastUpdateTime = 0;
+    var logicDt        = 1000/60;
+    var lag            = 0;
+
+    function update(currentUpdateTime) {
+        requestAnimationFrame(update);
+
+        dt = currentUpdateTime - lastUpdateTime;
+        lag += dt;
+
+        // Ensures that logic updates will be fixed
+        while (lag > logicDt){
+            lib_.logicUpdate(logicDt); // Call logic uptade
+            lag -= logicDt;
+        }
+
+        lastUpdateTime = currentUpdateTime
+
+        // render the stage
+        renderer.render(stage);
+    }
+    requestAnimationFrame(update);
+}
