8f57a640510215b3ac77d0710b43a9e8003688a6
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 20:43:56 -0300

    Adding rectangle.

--- src/library/lib.utils.js
+++ src/library/lib.utils.js
@@ -8,3 +8,23 @@ lib_.utils.removeFromArray = function(array, element) {
     if (index >= 0)
         array.splice(index, 1);
 }
+
+
+// Thanks to http://stackoverflow.com/questions/22073350/draw-a-rectangle-with-pixi-js
+lib_.utils.newRectangle = function(x, y, width, height, params) {
+    params = params || {}
+
+    var graphics = new PIXI.Graphics();
+
+    if (params.color != undefined) {
+        graphics.beginFill(params.color);
+    }
+
+    // set the line style to have a width of 5 and set the color to red
+    graphics.lineStyle(params.strokeWidth || 2, params.strokeColor || 0x000000);
+
+    // draw a rectangle
+    graphics.drawRect(x, y, width, height);
+
+    return graphics;
+}
