9a60c5bdef188785267c887492b502e4ed526e24
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 20:44:11 -0300

    Adding first showcase.

--- src/Main.js
+++ src/Main.js
@@ -10,3 +10,20 @@ lib_.setup()
 
 // Activate Window Resize
 lib_.windowUtils.setResize()
+
+
+// begin showcase
+
+lib_.physics.setGravity({
+    x : 0,
+    y : 0.01
+})
+
+var rect = lib_.utils.newRectangle(centerX, centerY, 20, 20, {
+    color : 0x00b300
+})
+stage.addChild(rect)
+
+lib_.physics.addBody(rect, {
+
+})
