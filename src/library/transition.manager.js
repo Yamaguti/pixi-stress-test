7bb8de9061d6aef0c32f117b2796fccb2a03285f
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 20:03:32 -0300

    Improving legibility in transitionManager.

--- src/library/transition.manager.js
+++ src/library/transition.manager.js
@@ -9,38 +9,40 @@ var TransitionManager = {}
 
 function startTransition(object, originalParams) {
     var animationParams = JSON.parse(JSON.stringify(originalParams));
-    var totalTime       = animationParams.time
-    var startTime       = TimerManager.getTime()
-    var easing          = Easings[animationParams.easing || "linear"]
+    var totalTime       = animationParams.time;
+    var startTime       = TimerManager.getTime();
+    var easing          = Easings[animationParams.easing || "linear"];
 
-    delete animationParams["time"]
-    delete animationParams["easing"]
+    delete animationParams["time"];
+    delete animationParams["easing"];
 
-    var startValues = {}
-    for (var transitionParam in animationParams) {
-        startValues[transitionParam] = object[transitionParam]
-    }
+    var startValues = {};
+    for (var transitionParam in animationParams)
+        startValues[transitionParam] = object[transitionParam];
 
     function animationHandler() {
         var currentValue = TimerManager.getTime()
 
         for (var param in animationParams) {
-            var result = easing(currentValue-startTime, startValues[param], animationParams[param]-startValues[param], totalTime)
-            object[param] = result
+            var result = easing(currentValue-startTime, startValues[param], animationParams[param]-startValues[param], totalTime);
+            object[param] = result;
         }
 
-        if (currentValue < startTime + totalTime) {
+        if (currentValue < startTime + totalTime)
             requestAnimationFrame(animationHandler);
 
-        } else { // end of transition
-            if (originalParams.onComplete) {
-                originalParams.onComplete()
-            }
-            for (var param in animationParams) {
-                object[param] = animationParams[param]
-            }
+        // end of transition
+        else {
+            // on complete callback, if any
+            if (originalParams.onComplete)
+                originalParams.onComplete();
+
+            // Set values to final value for higher precision
+            for (var param in animationParams)
+                object[param] = animationParams[param];
         }
     }
+
     requestAnimationFrame(animationHandler);
 }
 
