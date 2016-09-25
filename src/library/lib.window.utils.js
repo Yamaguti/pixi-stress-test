9ccb3a4cc64a8a39c5c46a5dc8cbee5a6d2b276b
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 18:14:11 -0300

    Adding window utils.

---
+++ src/library/lib.window.utils.js
@@ -0,0 +1,14 @@
+
+lib_.windowUtils = {}
+
+
+// Resizing renderer to center of browser window. Credits: http://www.html5gamedevs.com/topic/18406-how-to-center-stage-on-browser/
+lib_.windowUtils.setResize = function() {
+    var resizeFunction = function() {
+        renderer.view.style.position = 'absolute';
+        renderer.view.style.left = ((window.innerWidth - renderer.width) >> 1) + 'px';
+        renderer.view.style.top  = ((window.innerHeight - renderer.height) >> 1) + 'px';
+    }
+    resizeFunction();
+    window.addEventListener('resize', resizeFunction);
+}
