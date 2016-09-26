fb0907ca41044faaacb1939a95c02c22cb4cf7ef
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-09-25 21:18:30 -0300

    Updating showcase.

--- src/Main.js
+++ src/Main.js
@@ -14,25 +14,28 @@ lib_.windowUtils.setResize()
 
 // begin showcase
 
-lib_.physics.setGravity({
+var utils = lib_.utils
+var physics = lib_.physics
+
+physics.setGravity({
     x : 0.03,
     y : 0.01
 })
 
+var onClick = function(mousedata) {
+    for (var index = 1; index < 100; index++) {
+        var rect = utils.newRectangle(mousedata.x-15, mousedata.y-130, 5, 5, {
+            color : utils.newVibrantRandomColor(Math.random(), 10),
+            strokeWidth : 1
+        })
+        stage.addChild(rect)
 
-var utils = lib_.utils
-for (var index = 1; index < 1000; index++) {
-    var rect = utils.newRectangle(Math.random() * screenRight, Math.random() * screenBottom, 5, 5, {
-        color : utils.newVibrantRandomColor(Math.random(), 10),
-        strokeWidth : 1
-    })
-    stage.addChild(rect)
-
-    // physics showcase
-    lib_.physics.addBody(rect, { })
-    rect.physicsObject.xSpeed = ((Math.random()*2)-1)*0.05
-    rect.physicsObject.ySpeed = ((Math.random()*2)-1)*0.05
+        // physics showcase
+        lib_.physics.addBody(rect, { })
+        rect.physicsObject.xSpeed = ((Math.random()*2)-1)*0.05
+        rect.physicsObject.ySpeed = ((Math.random()*2)-1)*0.05
+    }
 }
 
-
-
+stage.interactive = true;
+stage.on('mousedown', onClick);
