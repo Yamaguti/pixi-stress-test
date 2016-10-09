a212e7abd8fc579020b1344a3537cfa8ea009e77
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-10-09 17:55:20 -0300

    Updating global stage.

--- src/library/lib.main.js
+++ src/library/lib.main.js
@@ -6,6 +6,7 @@ var lib_ = {}
 lib_.setup = function() {
     // Creates global stage
     stage = new PIXI.Container();
+    lib_.stage = stage
 
     // Creates a global renderer instance.
     renderer = PIXI.autoDetectRenderer(screenWidth, screenHeight, {backgroundColor : 0x1099bb});
