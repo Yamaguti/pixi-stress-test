6c2f6aea3c7c071cda69e83bf748111c90d524eb
Felipe Yamaguti <yamaguti.felipe@gmail.com>
2016-10-12 20:59:41 -0300

    Increasing stage render space to window size

--- src/library/lib.constants.js
+++ src/library/lib.constants.js
@@ -1,6 +1,11 @@
 
-var screenWidth  = 960;
-var screenHeight = 540;
+// var screenWidth  = 960;
+// var screenHeight = 540;
+
+var screenWidth  = window.innerWidth -10;
+var screenHeight = window.innerHeight -10;
+
+
 var centerX      = screenWidth/2;
 var centerY      = screenHeight/2;
 var screenBottom = screenHeight;
